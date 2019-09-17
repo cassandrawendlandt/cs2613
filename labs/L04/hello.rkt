9de6f31d3cbe96827c4d535284e27bdeddd5e0e0
Cassandra <cwendlan@unb.ca>
2019-09-17 16:37:44 -0300

    Adding the lab 4 folder with all completed code from the lab

---
+++ labs/L04/hello.rkt
@@ -0,0 +1,15 @@
+(module hello racket
+  (provide hello)
+  (define (hello)
+    (displayln "Hello world!"))
+  
+(module+ test
+  (require rackunit)
+  (check-equal? (with-output-to-string hello) "Hello world!\n")
+   (check-equal? (with-output-to-string hello) (begin (sleep 3) "Hello world!\n")))
+  )
+
+
+
+
+  
