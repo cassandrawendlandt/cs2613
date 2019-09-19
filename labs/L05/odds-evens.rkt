c1b004f7fb412f2b4eb3250914bbe6e93cffc89c
Cassandra <cwendlan@unb.ca>
2019-09-19 12:12:34 -0300

    Adding an updated version of odds-evens with the code from part 2 of lab 5
    The following methods was added: odds-even3 and big-list
    The last (module + test) was also added

--- labs/L05/odds-evens.rkt
+++ labs/L05/odds-evens.rkt
@@ -1,32 +1,49 @@
 #lang racket
 
-    (module+ test
-      (require rackunit))
+(module+ test
+  (require rackunit))
 
-    (define (odds-evens lst)
-      (cond
-        [(empty? lst) (list 0 0)]
-        [(odd? (first lst)) (map + '(1 0) (odds-evens (rest lst)))]
-        [(even? (first lst)) (map + '(0 1) (odds-evens (rest lst)))]))
+(define (odds-evens lst)
+  (cond
+    [(empty? lst) (list 0 0)]
+    [(odd? (first lst)) (map + '(1 0) (odds-evens (rest lst)))]
+    [(even? (first lst)) (map + '(0 1) (odds-evens (rest lst)))]))
 
-    (module+ test
-      (check-equal? (odds-evens (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4)))
-
-
+(module+ test
+  (check-equal? (odds-evens (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4)))
 
 
-    (define (odds-evens2 lst)
-      (define (helper lst odds evens)
-        (cond
-          [(empty? lst) (list odds evens)]
-          [(odd? (first lst)) (helper (rest lst) (+ odds 1) evens     )]
-          [(even? (first lst)) (helper (rest lst)  odds  (+ 1 evens ))]))
-      (helper lst 0 0))
+(define (odds-evens2 lst)
+  (define (helper lst odds evens)
+    (cond
+      [(empty? lst) (list odds evens)]
+      [(odd? (first lst)) (helper (rest lst) (+ odds 1) evens     )]
+      [(even? (first lst)) (helper (rest lst)  odds  (+ 1 evens ))]))
+   (helper lst 0 0))
 
-    (module+ test
-      (require rackunit)
-      (define random-list (build-list 100 (lambda (x) (random 1 100))))
-      (check-equal? (odds-evens2 (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
-      (check-equal? (odds-evens2 random-list) (odds-evens2 random-list))
-      )
+(module+ test
+  (require rackunit)
+  (define random-list (build-list 100 (lambda (x) (random 1 100))))
+  (check-equal? (odds-evens2 (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
+  (check-equal? (odds-evens random-list) (odds-evens2 random-list)))
+
+
+(define (odds-evens3 lst)
+  (for/fold
+      ([accumulator (list 0 0)])
+      ([n lst])
+    (cond
+       [(odd? n) (map + '(1 0) accumulator)]
+       [(even? n) (map + '(0 1) accumulator)])))
+
+(define big-list (range 1000000))
+(for* ([fun (list odds-evens odds-evens2 odds-evens3)]
+       [rep '(1 2 3)])
+  (printf "~a ~a\n" fun rep)
+  (time (fun big-list)))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (odds-evens3 (list 3 2 1 1 2 3 4 5 5 6)) (list 6 4))
+  (check-equal? (odds-evens random-list) (odds-evens3 random-list)))
 
