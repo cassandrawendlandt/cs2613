1f48bc97aee25a41f46760d8055f189f42cd9770
Cassandra <cwendlan@unb.ca>
2019-10-22 12:47:06 -0300

    Committing questions 3

---
+++ midterm/q3/person.js
@@ -0,0 +1,16 @@
+
+class Person {
+    constructor(name ,age){
+        this.name = name; 
+        this.age = age; 
+    }
+
+    birthday(){
+        this.name = this.name; 
+        this.age = this.age;
+        
+        return new Person("new"+this.name, this.age+1);
+    }
+}
+
+exports.Person = Person;
---
+++ midterm/q3/spec/person.spec.js
@@ -0,0 +1,23 @@
+let Person = require("../person.js").Person;
+
+describe("person",
+    function () {
+        let bob=new Person("bob", 42);
+        it("constructor",
+            function () {
+                expect(bob.name).toEqual("bob");
+                expect(bob.age).toEqual(42);
+
+            });
+
+        it("birthday does not mutate",
+            function (){
+                
+                let newbob = bob.birthday();
+                console.log(bob.name);
+
+                expect(bob.age).toEqual(42);
+                expect(newbob.age).toEqual(43);
+            });
+             
+    });
