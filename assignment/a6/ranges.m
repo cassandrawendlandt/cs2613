6725dcee61a285e880dcdcda6cad21d7cac4598c
cwendlan <cwendlan@unb.ca>
2019-12-05 08:07:38 -0400

    adding question 2 to finshed solution

---
+++ assignment/a6/ranges.m
@@ -0,0 +1,21 @@
+function [out] = ranges(array)
+  out =[min(array);max(array)];
+endfunction
+
+
+
+%!test
+%! A=[0,0,0;
+%!    0,1,0;
+%!    0,0,1;
+%!    1,1,1];
+%! assert (ranges(A), [0,0,0;
+%!                     1,1,1]);
+
+%!test
+%! A=[0,0,0;
+%!    0,10,0;
+%!    0,0,1;
+%!    1,1,-11];
+%! assert (ranges(A), [0,0,-11;
+%!                     1,10,1]);
